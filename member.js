633739eca36e53611f9ec7937c0898ceab4d181a
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-03-04 23:50:36 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  return {
+    skills: ["JavaScript", "React", "Node", "Express", "MongoDB"],
+    bio: "I am a full-stack developer with a passion for learning new technologies.",
+    experience: "I have been working as a developer for 5 years.",
+    projects:
+      "I have worked on a variety of projects, including e-commerce sites and web applications.",
+    contact: "You can reach me at",
+  };
+}
